3d6575ffddef7a924febf88781dd42f721dd7070
Daniel Hall <daniel@danielhall.me>
2012-05-31 00:17:48 +1000

    Some simple screens

--- metricsathome/BlackScreen.py
+++ metricsathome/BlackScreen.py
@@ -1,20 +1,20 @@
 
 import Image
 
-class WhiteScreen:
+class BlackScreen:
   def __init__(self):
     self._frameno = 0
 
   def getInfo(self):
     return {
       'duration':     5,
-      'name':         'White Screen',
+      'name':         'Black Screen',
       'framesleep':   1000,
     }
 
   def getImage(self, width, height):
     if self._frameno == 0:
-        return Image.new('RGB', (width, height), (255, 255, 255))
+        return Image.new('RGB', (width, height), (0, 0, 0))
         self._frameno += 1
     else:
         return None
