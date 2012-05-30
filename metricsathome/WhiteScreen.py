69b479240cb08c0f1d04b0ea0102d731aa5e6d71
Daniel Hall <daniel@danielhall.me>
2012-05-30 22:21:25 +1000

    Beginning framework

---
+++ metricsathome/WhiteScreen.py
@@ -0,0 +1,12 @@
+
+class WhiteScreen:
+  def __init__(self):
+    pass
+
+  def getinfo(self):
+    return {
+      'duration':     5,
+      'name':         'White Screen',
+      'framesleep':   1000,
+    }
+
