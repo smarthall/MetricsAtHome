50c237cdda31cf62dcfe82094300e297f153d54c
Daniel Hall <daniel@danielhall.me>
2012-05-31 21:03:37 +1000

    Started refactoring the ScreenController

--- metricsathome/ScreenController.py
+++ metricsathome/ScreenController.py
@@ -9,28 +9,49 @@ import metricsathome.HelloWorldScreen
 #import metricsathome.SamsungUSBDriver
 import metricsathome.X11Driver
 
-def run():
-  d = metricsathome.X11Driver.X11Driver()
-  
-  screens = [
-    metricsathome.HelloWorldScreen.HelloWorldScreen,
-    metricsathome.BlackScreen.BlackScreen,
-    metricsathome.WhiteScreen.WhiteScreen,
-    ]
+class ScreenController:
+  def __init__(self):
+    self._num = 0
 
-  di = d.getInfo()
+  def go(self):
+    dev = self.getDevice()
+    screens = self.getScreens()
 
-  for s in screens:
-    cur = s()
-    si = cur.getInfo()
-    print 'Screen: %s' % si['name']
+    while dev.devicePresent():
+      cur = self.getNextScreen(screens)
+      si = cur.getInfo()
+      self.showScreen(cur, dev)
+
+  def showScreen(self, screen, dev):
+    di = dev.getInfo()
+    si = screen.getInfo()
     quitntime = int(time.time() + si['duration'])
-    while (quitntime > time.time()) and (d.devicePresent()):
-      frame = cur.getImage(di['width'], di['height'])
+    while (quitntime > time.time()) and (dev.devicePresent()):
+      frame = screen.getImage(di['width'], di['height'])
       if not frame == None:
-        d.showFrame(frame)
-      if not d.devicePresent():
-        sys.exit(0)
+        dev.showFrame(frame)
+
+  def getNextScreen(self, screens):
+    self._num = (self._num + 1) % len(screens)
+    return screens[self._num]()
+
+  def getDevice(self):
+    return metricsathome.X11Driver.X11Driver()
+
+  def getScreens(self):
+    return [
+      metricsathome.HelloWorldScreen.HelloWorldScreen,
+      metricsathome.BlackScreen.BlackScreen,
+      metricsathome.WhiteScreen.WhiteScreen,
+      ]
+
+
+
+
+
+def run():
+  sc = ScreenController();
+  sc.go()
 
 
 
