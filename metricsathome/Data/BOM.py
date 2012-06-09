a054ed24f0627cea2c4be30ba0067d93da734313
Daniel Hall <daniel@danielhall.me>
2012-06-09 20:54:43 +1000

    Can now generate the radar background from transparencies

--- metricsathome/Data/BOM.py
+++ metricsathome/Data/BOM.py
@@ -5,21 +5,27 @@ import dateutil.parser
 from datetime import timedelta
 from cStringIO import StringIO
 import Cache
+from ftplib import FTP
+
 
 class BOM:
+  deflayers = ['background', 'topography', 'range',
+               'locations', 'copyright', 'legend.0']
+
   def __init__(self):
     self._xmlurl = 'ftp://ftp2.bom.gov.au/anon/gen/fwo/IDV10753.xml'
     self._radarbaseurl = 'ftp://ftp2.bom.gov.au/anon/gen/radar/IDR'
-    self._fcasts = None
+    self._ftphost = 'ftp2.bom.gov.au'
+    self._transparentcypath = '/anon/gen/radar_transparencies/'
     self._radarimg = None
 
   def getData(self, aac):
-    self._fcasts = Cache.read('metricsathome.BOM.BOM-data(' + aac + ')');
-    if self._fcasts is not None:
-      return self._fcasts
+    forecasts = Cache.read('metricsathome.BOM.BOM-data(' + aac + ')');
+    if forecasts is not None:
+      return forecasts
     
-    self._wdom = parse(urllib2.urlopen(self._xmlurl))
-    areas = self._wdom.getElementsByTagName('area')
+    wdom = parse(urllib2.urlopen(self._xmlurl))
+    areas = wdom.getElementsByTagName('area')
     area = filter(lambda a: a.attributes['aac'].value == aac, areas)[0]
     fcasts = filter(lambda a: a.attributes != None, area.childNodes)
     forecasts = []
@@ -32,6 +38,7 @@ class BOM:
         values[e.attributes['type'].value] = e.firstChild.nodeValue
       forecasts.append(values)
     forecasts = sorted(forecasts, key=lambda f: f['date'])
+
     Cache.write('metricsathome.BOM.BOM-data(' + aac + ')', forecasts, 3600)
     return forecasts
 
@@ -43,6 +50,45 @@ class BOM:
       self._radar = img_file.read()
       Cache.write('metricsathome.BOM.BOM-radar(' + code + ')', self._radar, 360)
 
-    imdata = StringIO(self._radar)
-    return Image.open(imdata)
+    return Image.open(StringIO(self._radar))
+
+  def getRadarLoop(self, code):
+    loopback = self.buildLoopBack(code)
+
+  def buildLoopBack(self, code, layers=deflayers):
+    cachekey = 'metricsathome.BOM.BOM-loopback(' + code + ',(' + ','.join(layers) + '))'
+    
+    background = Cache.read(cachekey);
+    if background is not None:
+      return Image.open(StringIO(background))
+
+    im = Image.new('RGBA', (524, 564), (255, 255, 255))
+
+    ftp = FTP(self._ftphost)
+    ftp.login()
+    ftp.cwd(self._transparentcypath)
+    files = ftp.nlst('IDR' + code + '.*.png')
+    files += ftp.nlst('IDR.*.png')
+
+    alllayers = {}
+    for f in files:
+       b = f.find('.') + 1
+       e = f.rfind('.')
+       alllayers[f[b:e]] = f
+
+    for l in layers:
+      imgout = StringIO()
+      ftp.retrbinary('RETR ' + alllayers[l], imgout.write)
+      imgin = StringIO(imgout.getvalue())
+      img = Image.open(imgin).convert('RGBA')
+      im.paste(img, (0,0), img)
+      imgout.close()
+      imgin.close()
+
+    imgout = StringIO()
+    im.save(imgout, format='PNG')
+    Cache.write(cachekey, imgout.getvalue(), 604800) # cache for a week
+    imgout.close()
+
+    return im
 
