52b53a7925f3a082434ca6d42bfd739dbdf0f841
Daniel Hall <daniel@danielhall.me>
2012-06-16 22:07:13 +1000

    Configuring the hello world screen

--- metricsathome/HelloWorldScreen.py
+++ metricsathome/HelloWorldScreen.py
@@ -6,9 +6,9 @@ class HelloWorldScreen:
     self.height = height
     self._xpos = 0
     self._ypos = 0
-    self._xdir = 4
-    self._ydir = 4
-    self._text = args.get('test', 'Hello World!')
+    self._xdir = 2
+    self._ydir = 2
+    self._text = args.get('text', 'Hello World!')
     self._textcolor = (0, 0, 0)
     self._font =  ImageFont.truetype('font/DejaVuSans.ttf', 96)
     (self._textw, self._texth) = self._font.getsize(self._text)
@@ -19,16 +19,16 @@ class HelloWorldScreen:
     
 
     self._xpos = (self._xpos + self._xdir)
-    if self._xpos >= (width - self._textw):
-      self._xpos = (width - self._textw) - 1
+    if self._xpos >= (self.width - self._textw):
+      self._xpos = (self.width - self._textw) - 1
       self._xdir *= -1
     elif self._xpos <= 0:
       self._xpos = 0
       self._xdir *= -1
 
     self._ypos = (self._ypos + self._ydir)# % (height - texth)
-    if self._ypos >= (height - self._texth):
-      self._ypos = (height - self._texth) - 1
+    if self._ypos >= (self.height - self._texth):
+      self._ypos = (self.height - self._texth) - 1
       self._ydir *= -1
     elif self._ypos <= 0:
       self._ypos = 0
