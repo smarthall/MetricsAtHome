942bee6ef07f33463f0ea0686c37c1b296e2d873
Daniel Hall <daniel@danielhall.me>
2012-07-01 18:09:11 +1000

    Pictures can now be configured

--- metricsathome/PicScreen.py
+++ metricsathome/PicScreen.py
@@ -5,11 +5,12 @@ import BaseScreen
 class PicScreen(BaseScreen.BaseScreen):
   def __init__(self, width, height, args):
     self._frameno = 0
+    self.picture = args.get('picture', 'img/sample.jpg')
 
   def getImage(self):
     if self._frameno == 0:
         self._frameno += 1
-        return Image.open('img/sample.jpg')
+        return Image.open(self.picture)
     else:
         time.sleep(1)
         return None
