f284ae33c8e7d65477bbf55bdfd5f2bfeb1e9e73
Daniel Hall <daniel@danielhall.me>
2012-07-01 20:37:48 +1000

    Resize, keep scale, add black bands

--- metricsathome/PicScreen.py
+++ metricsathome/PicScreen.py
@@ -1,16 +1,27 @@
 import time
 import Image
 import BaseScreen
+import FancyDraw
 
 class PicScreen(BaseScreen.BaseScreen):
   def __init__(self, width, height, args):
     self._frameno = 0
     self.picture = args.get('picture', 'img/sample.jpg')
+    self.width = width
+    self.height = height
+    self.im = Image.new('RGBA', (width, height), (0, 0, 0))
+
+    srcim = Image.open(self.picture).convert('RGBA')
+    ratio = min(float(width) / srcim.size[0], float(height) / srcim.size[1])
+    srcim = srcim.resize((int(srcim.size[0] * ratio),
+                          int(srcim.size[1] * ratio)), Image.ANTIALIAS)
+    draw = FancyDraw.FancyDraw(self.im)
+    draw.cpaste(srcim, (width / 2, height / 2), center='both')
 
   def getImage(self):
     if self._frameno == 0:
         self._frameno += 1
-        return Image.open(self.picture)
+        return self.im
     else:
         time.sleep(1)
         return None
