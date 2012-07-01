0d8d8c92a3ec91511958b13b6a81cba741c54309
Daniel Hall <daniel@danielhall.me>
2012-07-01 14:31:04 +1000

    Add a class for more advanced drawing

---
+++ metricsathome/FancyDraw.py
@@ -0,0 +1,29 @@
+import ImageDraw
+
+class FancyDraw(ImageDraw.ImageDraw):
+  def __init__(self, image):
+    self._im = image
+    ImageDraw.ImageDraw.__init__(self, image)
+
+  def _getcentering(self, axis):
+    if axis == 'both':
+      return (True, True)
+    elif axis.startswith('horiz'):
+      return (True, False)
+    elif axis.startswith('vert'):
+      return (False, True)
+
+  def ctext(self, pos, text, **kwargs):
+    (x, y) = pos
+    (ch, cv) = self._getcentering(kwargs.pop('center', 'none'))
+
+    if ch:
+      x -= self.textsize(text, font=kwargs.get('font'))[0] / 2
+    if cv:
+      y -= self.textsize(text, font=kwargs.get('font'))[1] / 2
+
+    return self.text((x, y), text, **kwargs)
+
+
+
+
