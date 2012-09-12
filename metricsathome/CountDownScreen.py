e9865cbff5c7f80b74bd58cf0839ae92a41d5b5e
Daniel Hall <daniel.hall@rea-group.com>
2012-09-12 18:14:10 +1000

    Countdown Screen done :D

--- metricsathome/CountDownScreen.py
+++ metricsathome/CountDownScreen.py
@@ -1,19 +1,43 @@
 import time, datetime
+import dateutil.parser
+from dateutil.tz import *
 import Image, ImageFont
 import BaseScreen
 import FancyDraw
 
 class CountDownScreen(BaseScreen.BaseScreen):
   def __init__(self, width, height, args):
+    # init
     self._nextframe = 0
+
+    # screen data
     self.width = width
     self.height = height
+
+    # Config
+    self.eventtitle = args.get('event', 'Something!')
+    self.strdatetime = args.get('datetime', 'Mon Dec 24 00:00:00 EST 2012')
+
+    # Colors
     self.color = self.HTMLColorToPILColor(args.get('color', '#000000'))
     self.textcolor = self.HTMLColorToPILColor(args.get('textcolor', '#FFFFFF'))
-    self.fontfile = args.get('fontfile', 'font/DejaVuSans.ttf')
-    self.timefontsize = args.get('timefontsize', 120)
 
+    # Standard fonts
+    self.fontfile = args.get('fontfile', 'font/DejaVuSans.ttf')
+    self.fontsize = args.get('fontsize', 48)
+    self.timefontsize = args.get('timefontsize', 80)
+    self.timelabelfontsize = args.get('timelabelfontsize', 48)
+
+    # Title font
+    self.titlefontfile = args.get('titlefontfile', 'font/DejaVuSans.ttf')
+    self.titlefontsize = args.get('titlefontsize', 100)
+
+    # Process options
+    self.datetime = dateutil.parser.parse(self.strdatetime)
+    self.font = ImageFont.truetype(self.fontfile, self.fontsize)
     self.timefont = ImageFont.truetype(self.fontfile, self.timefontsize)
+    self.timelabelfont = ImageFont.truetype(self.fontfile, self.timelabelfontsize)
+    self.titlefont = ImageFont.truetype(self.titlefontfile, self.titlefontsize)
 
   def HTMLColorToPILColor(self, colorstring):
     """ converts #RRGGBB to PIL-compatible integers"""
@@ -28,17 +52,49 @@ class CountDownScreen(BaseScreen.BaseScreen):
   def getImage(self):
     if time.time() > self._nextframe:
         self._nextframe = time.time() + 1
-        timestr = datetime.datetime.now().strftime(self.timeformat)
-        datestr = datetime.datetime.now().strftime(self.dateformat)
+        timedelta = self.datetime - datetime.datetime.now(tzutc())
         im = Image.new('RGB', (self.width, self.height), self.color)
         draw = FancyDraw.FancyDraw(im)
-        x = self.width / 2
-        timey = self.height / 2
-        datey = self.height / 2 + self.height / 8
-        draw.ctext((x, timey), timestr, font=self.timefont,
-                   fill=self.textcolor, center='both')
-        draw.ctext((x, datey), datestr, font=self.datefont,
-                   fill=self.textcolor, center='both')
+        titlex = self.width / 2
+        draw.ctext((titlex, 0), "Countdown to", font=self.font,
+                   fill=self.textcolor, center='horizontal')
+        draw.ctext((titlex, self.fontsize + 10), self.eventtitle, font=self.titlefont,
+                   fill=self.textcolor, center='horizontal')
+
+        xsector = self.width / 8
+        ttlsec = int(timedelta.total_seconds())
+        secs = ttlsec % 60
+        ttlsec -= secs
+        mins = ttlsec % 3600 / 60
+        ttlsec -= mins
+        hours = ttlsec % 86400 / 3600
+        ttlsec -= hours
+        days = ttlsec / 86400
+
+        # Days
+        draw.ctext((xsector * 1, 300), str(days), font=self.timefont,
+                   fill=self.textcolor, center='horizontal')
+        draw.ctext((xsector * 1, 370), "days", font=self.timelabelfont,
+                   fill=self.textcolor, center='horizontal')
+
+        # Hours
+        draw.ctext((xsector * 3, 300), str(hours), font=self.timefont,
+                   fill=self.textcolor, center='horizontal')
+        draw.ctext((xsector * 3, 370), "hours", font=self.timelabelfont,
+                   fill=self.textcolor, center='horizontal')
+
+        # Minutes
+        draw.ctext((xsector * 5, 300), str(mins), font=self.timefont,
+                   fill=self.textcolor, center='horizontal')
+        draw.ctext((xsector * 5, 370), "minutes", font=self.timelabelfont,
+                   fill=self.textcolor, center='horizontal')
+
+        # Seconds
+        draw.ctext((xsector * 7, 300), str(secs), font=self.timefont,
+                   fill=self.textcolor, center='horizontal')
+        draw.ctext((xsector * 7, 370), "seconds", font=self.timelabelfont,
+                   fill=self.textcolor, center='horizontal')
+
         return im
 
     else:
